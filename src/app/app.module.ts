f4b4b16a8903a5cd056c97888991468a376944da
arafat-amin <arafatalafasy2014@gmail.com>
2024-05-04 18:14:19 +0300

    Splitting main routing to a new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
-import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
-
-const appRoutes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-]
 
 @NgModule({
   declarations: [
@@ -29,7 +18,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
